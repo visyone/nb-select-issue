23a871be016cec09d0993c7712dd175f585cc3a2
Puccio Andrea <andrea.puccio@futuryng.com>
2021-09-15 15:22:23 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NbOptionComponent, NbSelectComponent } from '@nebular/theme';
+import { isEqual } from 'lodash';
+
+interface SelectableItem {
+  id: number;
+  label: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nb-select-issue';
+  // static elemements
+  public selectedStaticItem = 1;
+  public staticItems: SelectableItem[] = [
+    {
+      id: 1,
+      label: 'Rome'
+    },
+    {
+      id: 2,
+      label: 'Paris'
+    },
+    {
+      id: 3,
+      label: 'London'
+    }
+  ];
+
+  // dynamic elements
+  public selectedDynamicItem: number | null = 1;
+  public dynamicItems: SelectableItem[] = [{
+    id: 1,
+    label: 'Audi'
+  },];
+  @ViewChild('dynamicSelect', { static: true }) dynamicSelect: NbSelectComponent | null = null;
+
+  /**
+   * Method suggested by @rardila-uniajc in nebular issue {@link https://github.com/akveo/nebular/issues/2145}
+   */
+  static setOptionNbSelect(selectComponent: NbSelectComponent | null, optToCompare: any) {
+    setTimeout(() => {
+      if (selectComponent) {
+        const selectedOptions: NbOptionComponent<SelectableItem>[] = [];
+        for (const option of selectComponent.options['_results']) {
+          if (isEqual(optToCompare, option['value'])) {
+            selectedOptions.push(option);
+            break;
+          }
+        }
+        for (const option of selectedOptions) {
+          selectComponent['selectOption'](option);
+        }
+        selectComponent['cd'].detectChanges();
+      }
+    }, 500);
+  }
+
+  public ngOnInit() {
+    // populate the select dynamically while selected item is already defined
+    setTimeout(() => {
+      this.dynamicItems = [
+        {
+          id: 1,
+          label: 'Ferrari'
+        },
+        {
+          id: 2,
+          label: 'Mercedes'
+        },
+        {
+          id: 3,
+          label: 'Lamborghini'
+        }
+      ];
+
+      /***************************************************** 
+       * uncomment out to resolve the issue with a workaround
+       * ***************************************************/ 
+      // this.refreshSelect();
+
+    }, 1500);
+
+  }
+
+  private refreshSelect() {
+    AppComponent.setOptionNbSelect(this.dynamicSelect, this.selectedDynamicItem);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbSelectModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbCardModule,
+    NbSelectModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
